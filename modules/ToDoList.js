f24d51aba12982a64a94c788d478785ced24609c
asnganje <abd.nganje@gmail.com>
2022-08-17 17:41:39 +0300

    add module toDoList

---
+++ modules/ToDoList.js
@@ -0,0 +1,23 @@
+const toDO = [
+ {
+    description: 'shopping',
+    completed: false,
+    index: 0,
+ },
+ {
+    description: 'cooking',
+    completed: false,
+    index: 1,
+ },
+ {
+    description: 'cleaning',
+    completed: false,
+    index: 2,
+ },
+ {
+    description: 'washing',
+    completed: false,
+    index: 3,
+ },
+];
+export default toDO;
